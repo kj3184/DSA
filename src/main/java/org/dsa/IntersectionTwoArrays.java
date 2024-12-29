66b4fe62bef97c94b408cacdee60945e5515ae69
kj3184 <kunal.jadhav84@gmail.com>
2024-12-29 18:52:59 +0800

    Added new line at the end of class

--- src/main/java/org/dsa/IntersectionTwoArrays.java
+++ src/main/java/org/dsa/IntersectionTwoArrays.java
@@ -1,4 +1,18 @@
 package org.dsa;
 
+import java.util.*;
+
 public class IntersectionTwoArrays {
+    public static ArrayList<Integer> intersectionWithoutDuplicates(int[] a, int[] b) {
+        Set<Integer> resultSet=new HashSet<>();
+        for (int i : a) {
+            for (int j : b) {
+                if (i == j) {
+                    resultSet.add(i);
+                    break;
+                }
+            }
+        }
+        return new ArrayList<>(resultSet);
+    }
 }
--- src/test/java/org/dsa/IntersectionTwoArraysTest.java
+++ src/test/java/org/dsa/IntersectionTwoArraysTest.java
@@ -1,4 +1,20 @@
+package org.dsa;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class IntersectionTwoArraysTest {
-  
+
+    @Test
+    void intersectionWithoutDuplicates() {
+        ArrayList<Integer> resList=new ArrayList<Integer>();
+        resList.add(1);
+        resList.add(3);
+        int[] a={1,2,1,3,1};
+        int[] b={3,1,3,4,1};
+        assertEquals(resList,IntersectionTwoArrays.intersectionWithoutDuplicates(a,b));
+    }
 }
